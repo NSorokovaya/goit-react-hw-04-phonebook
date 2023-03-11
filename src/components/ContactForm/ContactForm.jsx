01b3e11f52e9ea3584d71a008a5227e53029b7c0
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-03-11 09:23:13 -0500

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,66 +1,100 @@
-import { Component } from 'react';
+//import { Component } from 'react';
+import { useState } from 'react';
 import css from './ContactForm.module.css';
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
+
+// class ContactForm extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   handleNameChange = e => {
+//     this.setState({ name: e.currentTarget.value });
+//   };
+
+//   handleNumberChange = e => {
+//     this.setState({ number: e.currentTarget.value });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     this.props.onSubmit(this.state);
+//     this.reset();
+//   };
+
+//   reset = () => {
+//     this.setState({ name: '', number: '' });
+//   };
+export const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+
+
+
+const handleChange = evt => {
+    const { name, value } = evt.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleNameChange = e => {
-    this.setState({ name: e.currentTarget.value });
-  };
-
-  handleNumberChange = e => {
-    this.setState({ number: e.currentTarget.value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit({ name, number });
+
+    e.currentTarget.reset();
+     setName('')
+    setNumber('')
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className={css.contactForm}>
-        <label>
-          Name <br />
-          <input
-            className={css.input}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={this.state.name}
-            onChange={this.handleNameChange}
-          />
-        </label>
-        <br />
-        <label>
-          Number <br />
-          <input
-            className={css.input}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={this.state.number}
-            onChange={this.handleNumberChange}
-          />
-        </label>
-        <br />
-        <button className={css.btn} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
+
+
+  
+   return (
+    <form onSubmit={handleSubmit} className={css.contactForm}>
+      <label>
+        Name <br />
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Number <br />
+        <input
+          className={css.input}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button className={css.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
   }
-}
+
 
 export default ContactForm;
